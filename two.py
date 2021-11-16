fd7f80e2b80b503d3434e42db0ec82eeaafbe604
rramen <mr.ramonreyes11@gmail.com>
2021-11-16 14:00:33 -0600

    finally vector field

--- two.py
+++ two.py
@@ -1,23 +1,27 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-x,y = np.meshgrid(np.linspace(-5,5,20), np.linspace(-5,5,20))
+x,y = np.meshgrid(np.linspace(0,100,20), np.linspace(0,40,20))
+
 
 Lambda = 32.221
 b = 1.3847
 Mu = 27.432
 c = 0.5433
 
-u = x*(Lambda-(b*y))
-v = y*(-Mu+(c*x))
 
-equation = (u**2)/(b/(c*Lambda)) + (v**2)/(c/(b*Mu))
+dx = x*(Lambda-b*y)
+dy = y*(-Mu+c*x)
 
-plt.quiver(x,y,u,v)
+n = np.sqrt(dx**2 + dy**2)
+u,v = dx/n, dy/n
+
+plt.quiver(x,y,dx,dy)
+plt.show()
 
 x1,y1 = 21,44
 x2,y2 = 72,23
 x3,y3 = 2,16
 
-
-plt.show()
+equation = (u**2)/(b/(c*Lambda)) + (v**2)/(c/(b*Mu))
+equation2 = (-Mu*np.log(x)+c*x-Lambda*np.log(y)+b*y)
