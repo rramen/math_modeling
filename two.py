8293ce2574e879ae5a86cc4228ca7388771c6bde
rramen <mr.ramonreyes11@gmail.com>
2021-11-18 14:37:38 -0600

    formatting

--- two.py
+++ two.py
@@ -2,6 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.integrate.odepack import odeint
 
+##### problem 1 #####
+
 x,y = np.meshgrid(np.linspace(0,250,30), np.linspace(0,130,30))
 
 Lambda = 32.221
