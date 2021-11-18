85a5ce95c49419b69f02a2564113eb3770606bdc
rramen <mr.ramonreyes11@gmail.com>
2021-11-18 14:36:01 -0600

    ellipses plot added v2

--- two.py
+++ two.py
@@ -13,6 +13,27 @@ f1 = [21,44]
 f2 = [72,23]
 f3 = [2,16]
 
+def find_k(x,y,Lambda,b,Mu,c):
+    dotx = Mu/c
+    doty = Lambda/b
+    u = x-dotx
+    v = y-doty
+    k =  (u**2)/(b/(c*Lambda)) + (v**2)/(c/(b*Mu))
+    return k, dotx, doty
+
+U,V = np.meshgrid(np.linspace(-20,120,1000),np.linspace(-20,120,1000))
+
+k1,x1,y1 = find_k(f1[0], f1[1], Lambda,b,Mu,c)
+k2,x2,y2 = find_k(f2[0], f2[1], Lambda,b,Mu,c)
+
+equation1 = ((U-x1)**2)/(b/(c*Lambda)) + ((V-y1)**2)/(c/(b*Mu))
+equation2 = ((U-x2)**2)/(b/(c*Lambda)) + ((V-y2)**2)/(c/(b*Mu))
+plt.contour(U,V,equation1, [k1])
+plt.contour(U,V,equation2, [k2])
+plt.show()
+
+##### problem 2 #####
+
 dx = x*(Lambda-b*y)
 dy = y*(-Mu+c*x)
 t = np.linspace(0,1,1000)
@@ -35,22 +56,6 @@ plt.plot(sol2[:,0], sol2[:,1])
 plt.quiver(x,y,u,v)
 plt.show()
 
-def find_k(x,y,Lambda,b,Mu,c):
-    dotx = Mu/c
-    doty = Lambda/b
-    u = x-dotx
-    v = y-doty
-    k =  (u**2)/(b/(c*Lambda)) + (v**2)/(c/(b*Mu))
-    return k, dotx, doty
 
-U,V = np.meshgrid(np.linspace(-20,120,1000),np.linspace(-20,120,1000))
 
-k1,x1,y1 = find_k(f1[0], f1[1], Lambda,b,Mu,c)
-k2,x2,y2 = find_k(f2[0], f2[1], Lambda,b,Mu,c)
 
-equation1 = ((U-x1)**2)/(b/(c*Lambda)) + ((V-y1)**2)/(c/(b*Mu))
-equation2 = ((U-x2)**2)/(b/(c*Lambda)) + ((V-y2)**2)/(c/(b*Mu))
-plt.contour(U,V,equation1, [k1])
-plt.contour(U,V,equation2, [k2])
-plt.show()
-equation2 = (-Mu*np.log(x)+c*x-Lambda*np.log(y)+b*y)
